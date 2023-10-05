2c5f257f6cfac0d7072bbde728bb53877af07032
Gustavo Machado <gustavomf02@gmail.com>
2023-10-05 18:37:01 -0300

    Passa a usar dois iteradores, um pra cada caso

--- Trabalho 2/src/ibd/query/unaryop/DuplicateRemovalGustavoMachadoDeFreitas.java
+++ Trabalho 2/src/ibd/query/unaryop/DuplicateRemovalGustavoMachadoDeFreitas.java
@@ -1,18 +1,23 @@
 package ibd.query.unaryop;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import ibd.query.Operation;
 import ibd.query.OperationIterator;
 import ibd.query.Tuple;
 import ibd.query.lookup.LookupFilter;
+import ibd.query.lookup.RealLookupFilter;
+import ibd.query.unaryop.sort.PKSort;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DuplicateRemovalGustavoMachadoDeFreitas extends UnaryOperation {
     Boolean IsDataSorted;
 
-    ArrayList<String> uniqueContents;
+    ArrayList<Tuple> uniqueTuples;
 
     public DuplicateRemovalGustavoMachadoDeFreitas(Operation childOperation, String dataSourceAlias, boolean isDataSorted) throws Exception {
         super(childOperation, dataSourceAlias);
@@ -20,11 +25,17 @@ public class DuplicateRemovalGustavoMachadoDeFreitas extends UnaryOperation {
     }
 
     @Override
-    public Iterator<Tuple> lookUp(LookupFilter filter) {
-        return new DuplicateRemovalIterator(filter);
+    public Iterator<Tuple> lookUp(LookupFilter filter)
+    {
+        if (IsDataSorted)
+        {
+            return new SortedDuplicateRemovalIterator(filter);
+
+        }
+        return new UnsortedDuplicateRemovalIterator(filter);
     }
 
-    public class DuplicateRemovalIterator extends OperationIterator {
+    public class SortedDuplicateRemovalIterator extends OperationIterator {
         // the iterator over the child operation
         Iterator<Tuple> tupleIterator;
         // the lookup required from the parent operation
@@ -32,12 +43,9 @@ public class DuplicateRemovalGustavoMachadoDeFreitas extends UnaryOperation {
 
         String lastContent;
 
-        public DuplicateRemovalIterator(LookupFilter lookup) {
+        public SortedDuplicateRemovalIterator(LookupFilter lookup) {
             this.lookup = lookup;
-            if (uniqueContents == null && !IsDataSorted) {
-                uniqueContents = new ArrayList<>();
-            }
-            tupleIterator = childOperation.lookUp(lookup);//pushes filter down to the child operation
+            tupleIterator = childOperation.lookUp(lookup);
         }
 
         @Override
@@ -46,17 +54,70 @@ public class DuplicateRemovalGustavoMachadoDeFreitas extends UnaryOperation {
                 Tuple tp = tupleIterator.next();
                 String content = tp.sourceTuples[sourceTupleIndex].record.getContent();
 
-                if (lookup.match(tp)) {
-                    if (!IsDataSorted && !uniqueContents.contains(content)) {
-                        uniqueContents.add(content);
-                        return tp;
-                    }
+                if (lookup.match(tp) && !Objects.equals(content, lastContent))
+                {
+                    lastContent = content;
+                    return tp;
+                }
+            }
+            return null;
+        }
+    }
 
-                    if (IsDataSorted && !Objects.equals(content, lastContent))
+    public class UnsortedDuplicateRemovalIterator extends OperationIterator {
+        // the iterator over the child operation
+        Iterator<Tuple> tupleIterator;
+        // the lookup required from the parent operation
+        LookupFilter lookup;
+
+        public UnsortedDuplicateRemovalIterator(LookupFilter lookup) {
+            this.lookup = lookup;
+            if (uniqueTuples == null)
+            {
+                uniqueTuples = new ArrayList<>();
+                try
+                {
+                    tupleIterator = childOperation.run();
+                    while (tupleIterator.hasNext())
                     {
-                        lastContent = content;
-                        return tp;
+                       Tuple tuple = (Tuple) tupleIterator.next();
+                       if (IsUnique(tuple))
+                       {
+                           uniqueTuples.add(tuple);
+                       }
                     }
+                } catch (Exception ex) {
+                    Logger.getLogger(PKSort.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            tupleIterator = uniqueTuples.iterator();
+        }
+
+        private Boolean IsUnique(Tuple tuple)
+        {
+            String content = tuple.sourceTuples[sourceTupleIndex].record.getContent();
+
+            for (Tuple uniqueTuple : uniqueTuples)
+            {
+                String uniqueContent = uniqueTuple.sourceTuples[sourceTupleIndex].record.getContent();
+                if (Objects.equals(content, uniqueContent))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        @Override
+        protected Tuple findNextTuple() {
+            while (tupleIterator.hasNext())
+            {
+                Tuple tp = tupleIterator.next();
+
+                if (lookup.match(tp)) {
+                    return tp;
                 }
             }
             return null;
